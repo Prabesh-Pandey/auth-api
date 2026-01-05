66fc4aec5a90828d22d6989886d17cd2061c2120
Prabesh Pandey <prabesh.code@gmail.com>
2026-01-05 20:50:09 +0545

    bcrypt and jwt

---
+++ utils/jwt.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const generateAccessToken = (user) =>
+  jwt.sign(
+    { userId: user._id, role: user.role },
+    process.env.JWT_ACCESS_SECRET,
+    { expiresIn: "15m" }
+  );
+
+const generateRefreshToken = (user) =>
+  jwt.sign(
+    { userId: user._id },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: "7d" }
+  );
+
+module.exports = { generateAccessToken, generateRefreshToken };
