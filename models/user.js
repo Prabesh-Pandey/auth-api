edb3405fc138290d909e650b15a18f9425d92645
Prabesh Pandey <prabesh.code@gmail.com>
2026-01-05 20:24:49 +0545

    db connection and user schema

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+     {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false
+    },
+    role: {
+      type: String,
+      default: "user"
+    },
+    refreshToken: {
+      type: String
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model('User', userSchema);
